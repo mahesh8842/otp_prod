e25b284e9e4dc7300bce634a8c62c7889168c2a9
mahesh8842 <55652610+mahesh8842@users.noreply.github.com>
2022-12-27 13:00:35 +0530

    added deployment configs

--- otp/settings.py
+++ otp/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'public/static')
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
